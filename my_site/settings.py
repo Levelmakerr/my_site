346591cf81da2fd7ddaf8a579f2b6a97530c884f
Levelmakerr <vishnuak329@gmail.com>
2024-09-24 10:27:49 +0530

    first

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uccdnb4)hlqtk+2f4r%@0m$gy4$m((3t9htj%00lla+^=^zccu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    
+    getenv("APP_HOST")
+]
 
 
 # Application definition
